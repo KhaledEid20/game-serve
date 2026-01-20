9e3f1f4c02d298bce5899c33180d1aff227d7c1c
Khaled Eid <khaled_eid77@outlook.com>
2026-01-20 02:23:42 +0200

    fix the UDP Server Issue + Test

--- UDP-Server/Networking/UDPClientListner.cs
+++ UDP-Server/Networking/UDPClientListner.cs
@@ -1,43 +1,49 @@
 
+using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Channels;
-using Microsoft.Extensions.Options;
-using UDP_Server.Networking.Packets;
+    using System.Text;
+    using System.Text.Json;
+    using System.Threading.Channels;
+    using Microsoft.Extensions.Options;
+    using UDP_Server.Networking.Packets;
 
-namespace UDP_Server.Networking;
+    namespace UDP_Server.Networking;
 
-class UDPClientListner : BackgroundService
-{
-    private readonly Channel<RawPacket> _channel;
-    private readonly UDPOption _options;
-    public UDPClientListner(Channel<RawPacket> channel, IOptions<UDPOption> options)
+    class UDPClientListner : BackgroundService
     {
-        _channel = channel;
-        _options = options.Value;
-    }
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        using UdpClient listner = new UdpClient(_options.IpAdress , _options.Port);
-        Console.WriteLine($"UDP Client Listner Started on {_options.IpAdress}/{_options.Port}...");
-        while (!stoppingToken.IsCancellationRequested)
+        private readonly Channel<RawPacket> _channel;
+        private readonly UDPOption _options;
+        public UDPClientListner(Channel<RawPacket> channel, IOptions<UDPOption> options)
         {
-            UdpReceiveResult result;
-            try
-            {
-                result = await listner.ReceiveAsync(stoppingToken);
-            }
-            catch (OperationCanceledException)
-            {
-                break;
-            }   
-            byte[] buffer = result.Buffer;
-            string json = Encoding.UTF8.GetString(buffer);
-
-            RawPacket packet = JsonSerializer.Deserialize<RawPacket>(json);
-
-            await _channel.Writer.WriteAsync(packet, stoppingToken);
+            _channel = channel;
+            _options = options.Value;
         }
-    }   
-}
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var ip = IPAddress.Parse(_options.IpAdress);
+            using UdpClient listner = new UdpClient(new IPEndPoint(ip, _options.Port));
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                UdpReceiveResult result;
+                try
+                {
+                    result = await listner.ReceiveAsync(stoppingToken);
+
+                    Console.WriteLine("Message received from " + result.RemoteEndPoint.ToString());
+
+                    byte[] messageReceived = Encoding.ASCII.GetBytes("Message Received\n");
+                    await listner.SendAsync(messageReceived, messageReceived.Length, result.RemoteEndPoint);
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+                byte[] buffer = result.Buffer;
+                string json = Encoding.UTF8.GetString(buffer);
+
+                RawPacket packet = JsonSerializer.Deserialize<RawPacket>(json);
+
+                await _channel.Writer.WriteAsync(packet, stoppingToken);
+            }
+        }   
+    }
